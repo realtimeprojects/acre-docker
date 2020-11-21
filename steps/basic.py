70a2e39d1db71e758077d40706284698ce429741
Claudio Klingler <ck@realtime-projects.com>
2020-11-21 11:27:50 +0100

    add hello world test.

---
+++ steps/basic.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from radish import given, when, then
+
+
+@given("nothing")
+def nothing(step):
+    pass
+
+
+@then("I expect nothing")
+def i_expect_nothing(step):
+    pass
+
+
+@when("I say '{message}'")
+def i_say(step, message):
+    pass
