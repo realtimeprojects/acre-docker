ba463cc8a052444d3b4b0651f635cdd530c993a4
Claudio Klingler <ck@realtime-projects.com>
2020-11-24 20:31:14 +0100

    use world context word for webdriver, add "close browser" step.

--- steps/selenium/browser.py
+++ steps/selenium/browser.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-from radish import given, then, when
+from radish import given, then, when, step
 
 from radish import world
 
@@ -10,18 +10,23 @@ from radish import world
 @given("I start the chromium browser")
 def i_start_the_browser(step):
     desired = DesiredCapabilities.CHROME
-    step.context.webdriver = webdriver.Chrome(
+    world.webdriver = webdriver.Chrome(
             desired_capabilities=desired,
             options=world.chrome_options())
+
+
+@step("I close the browser")
+def i_close_the_browser(step):
+    world.webdriver.close()
 
 
 @when("I navigate to '{url}'")
 def i_navigate_to(step, url):
     print("opening url '{}'".format(url))
-    step.context.webdriver.get(url)
+    world.webdriver.get(url)
 
 
 @then("I see '{title}' in the page title")
 def i_see_the_title(step, title):
     print("checking title %s" % title)
-    world.asserts.contains(title, step.context.webdriver.title)
+    world.asserts.contains(title, world.webdriver.title)
